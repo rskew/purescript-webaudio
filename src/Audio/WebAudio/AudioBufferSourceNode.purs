dd3db60f60b080b5aceead4517a5a58277659495
John Watson <john.watson@gmx.co.uk>
2018-04-30 10:59:18 +0100

    Add document comments.

--- src/Audio/WebAudio/AudioBufferSourceNode.purs
+++ src/Audio/WebAudio/AudioBufferSourceNode.purs
@@ -2,38 +2,51 @@ module Audio.WebAudio.AudioBufferSourceNode
   ( setBuffer, startBufferSource, stopBufferSource
   , loop, setLoop, loopStart, setLoopStart, loopEnd, setLoopEnd  ) where
 
+-- | Audio Buffer Source Node.  This is an audio source consisting of in-memory
+-- | audio data, stored in an AudioBuffer.
+-- | See https://developer.mozilla.org/en-US/docs/Web/API/AudioBufferSourceNode.
+
 import Prelude
 import Control.Monad.Eff (Eff)
 import Audio.WebAudio.Types (AudioBuffer, AudioBufferSourceNode, AUDIO)
 import Audio.WebAudio.Utils (unsafeGetProp, unsafeSetProp)
 
+-- | Prime the node with its AudioBuffer.
 foreign import setBuffer
   :: ∀ eff. AudioBuffer
   -> AudioBufferSourceNode
   -> (Eff (audio :: AUDIO | eff) Unit)
 
+-- | Start playing the AudioBuffer.
 foreign import startBufferSource
   :: ∀ eff. Number
   -> AudioBufferSourceNode
   -> (Eff (audio :: AUDIO | eff) Unit)
 
+-- | Stop playing the AudioBuffer.
 foreign import stopBufferSource
   :: ∀ eff. Number
   -> AudioBufferSourceNode
   -> (Eff (audio :: AUDIO | eff) Unit)
 
+-- | Indicate that the AudioBuffer should be replayed from the start once its
+-- | end has been reached.
 loop :: ∀ eff. AudioBufferSourceNode -> (Eff (audio :: AUDIO | eff) Boolean)
 loop = unsafeGetProp "loop"
 
 setLoop :: ∀ eff. Boolean -> AudioBufferSourceNode -> (Eff (audio :: AUDIO | eff) Unit)
 setLoop l n = unsafeSetProp "loop" n l
 
+-- | The time, in seconds, at which playback of the AudioBuffer must begin when
+-- | loop is true (default 0).
 loopStart :: ∀ eff. AudioBufferSourceNode -> (Eff (audio :: AUDIO | eff) Number)
 loopStart = unsafeGetProp "loopStart"
 
 setLoopStart :: ∀ eff. Number -> AudioBufferSourceNode -> (Eff (audio :: AUDIO | eff) Unit)
 setLoopStart l n = unsafeSetProp "loopStart" n l
 
+-- | The time, in seconds, at which playback of the AudioBuffer must end when
+-- | loop is true (default 0).
 loopEnd :: ∀ eff. AudioBufferSourceNode -> (Eff (audio :: AUDIO | eff) Number)
 loopEnd = unsafeGetProp "loopEnd"
 
--- src/Audio/WebAudio/ConvolverNode.purs
+++ src/Audio/WebAudio/ConvolverNode.purs
@@ -1,15 +1,24 @@
 module Audio.WebAudio.ConvolverNode (setBuffer, normalize, isNormalized) where
 
+-- | Convolver Node.  This is usually used to provide Reverb - for example
+-- | by simulating the effect of hearing the sound being played in a hall
+-- | (termed the impulse response).
+-- | It achieves this by performing a Linear Convolution on a supplied
+-- | AudioBuffer which represent the hall's acoustics.
+-- | See https://developer.mozilla.org/en-US/docs/Web/API/ConvolverNode.
+
 import Prelude (Unit)
 import Control.Monad.Eff (Eff)
 import Audio.WebAudio.Types (AudioBuffer, ConvolverNode, AUDIO)
 import Audio.WebAudio.Utils (unsafeGetProp, unsafeSetProp)
 
+-- | Set the AudioBuffer of the impulse response.
 foreign import setBuffer
   :: ∀ eff. AudioBuffer
   -> ConvolverNode
   -> (Eff (audio :: AUDIO | eff) Unit)
 
+-- | If true, the impulse response will be scaled by an equal-power normalization
 normalize :: ∀ eff. Boolean -> ConvolverNode -> (Eff (audio :: AUDIO | eff) Unit)
 normalize l n = unsafeSetProp "normalize" n l
 
