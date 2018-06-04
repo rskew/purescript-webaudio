35a67e4e78fe699c9626c6abbfc961d4bab4eedb
Alex Kelley <adkelley@mac.com>
2018-06-04 16:32:56 -0700

    square-wave ported to v0.12

--- examples/square-wave/Main.purs
+++ examples/square-wave/Main.purs
@@ -3,30 +3,29 @@ module SquareWave where
 
 import Prelude
 
-import Audio.WebAudio.Types (AUDIO, AudioContext, GainNode, OscillatorNode, AudioContextState(..), connect)
-import Audio.WebAudio.BaseAudioContext (createGain, createOscillator, currentTime, destination, newAudioContext, resume, state, suspend)
 import Audio.WebAudio.AudioParam (getValue, setValue, setValueAtTime)
+import Audio.WebAudio.BaseAudioContext (createGain, createOscillator, currentTime, destination, newAudioContext, resume, state, suspend)
 import Audio.WebAudio.GainNode (gain)
 import Audio.WebAudio.Oscillator (OscillatorType(..), frequency, setOscillatorType, startOscillator)
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Exception (EXCEPTION, throw)
-import Control.Monad.Eff.Ref (REF, Ref, newRef, readRef, writeRef)
-import Control.Monad.Eff.Timer (IntervalId, TIMER, clearInterval, setInterval)
-import DOM (DOM)
-import DOM.Event.EventTarget (addEventListener, eventListener)
-import DOM.Event.Types (EventTarget)
-import DOM.HTML (window)
-import DOM.HTML.Types (htmlDocumentToParentNode)
-import DOM.HTML.Window (document)
-import DOM.Node.ParentNode (querySelector)
+import Audio.WebAudio.Types (AudioContext, GainNode, OscillatorNode, AudioContextState(..), connect)
 import Data.Maybe (Maybe(..))
 import Data.Newtype (wrap)
+import Effect (Effect)
+import Effect.Exception (throw)
+import Effect.Ref (Ref, new, read, write)
+import Effect.Timer (IntervalId, clearInterval, setInterval)
 import Unsafe.Coerce (unsafeCoerce)
+import Web.DOM.ParentNode (querySelector)
+import Web.Event.EventTarget (EventTarget, addEventListener, eventListener)
+import Web.HTML (window)
+import Web.HTML.HTMLDocument (toParentNode)
+import Web.HTML.Window (document)
 
-beep :: ∀ e. AudioContext
-     -> OscillatorNode
-     -> GainNode
-     -> Eff (audio :: AUDIO, timer :: TIMER | e) Unit
+beep
+  :: AudioContext
+  -> OscillatorNode
+  -> GainNode
+  -> Effect Unit
 beep ctx osc g = do
   freqParam <- frequency osc
   f <- getValue freqParam
@@ -38,31 +37,25 @@ beep ctx osc g = do
   _ <- setValueAtTime 0.001 (t + 0.2) gainParam
   pure unit
 
-controls ::
-     ∀ e. Ref IntervalId
-     -> AudioContext
-     -> OscillatorNode
-     -> GainNode
-     -> Eff (audio :: AUDIO, timer :: TIMER, ref :: REF | e) Unit
+controls
+  :: Ref IntervalId
+  -> AudioContext
+  -> OscillatorNode
+  -> GainNode
+  -> Effect Unit
 controls ref ctx osc g = do
   s <- state ctx
   if (s == SUSPENDED)
      then do
        resume ctx
        t <- setInterval 1000 $ beep ctx osc g
-       writeRef ref t
+       write t ref
      else do
        suspend ctx
-       val <- readRef ref
+       val <- read ref
        clearInterval val
 
-main ::
-  ∀ e. Eff ( audio :: AUDIO
-           , dom :: DOM
-           , exception :: EXCEPTION
-           , timer :: TIMER
-           , ref :: REF | e
-           ) Unit
+main :: Effect Unit
 main = do
   ctx <- newAudioContext
 
@@ -78,15 +71,19 @@ main = do
 
   suspend ctx
 
-  let id = unsafeCoerce (newRef 0) :: Ref IntervalId
+  let id = unsafeCoerce (new 0) :: Ref IntervalId
 
-  doc <- map htmlDocumentToParentNode (window >>= document)
+  doc <- map toParentNode (window >>= document)
   play <- querySelector (wrap "#play") doc
   case play of
-    Just e -> addEventListener (wrap "click") (eventListener \_ -> controls id ctx osc g) false (unsafeCoerce e :: EventTarget)
+    Just e -> do
+      el <- eventListener \_ -> controls id ctx osc g
+      addEventListener (wrap "click") el false (unsafeCoerce e :: EventTarget)
     Nothing -> throw "No 'play' button"
   stop <- querySelector (wrap "#stop") doc
   case stop of
-    Just e -> addEventListener (wrap "click") (eventListener \_ -> controls id ctx osc g) false (unsafeCoerce e :: EventTarget)
+    Just e -> do
+      el  ← eventListener \_ -> controls id ctx osc g
+      addEventListener (wrap "click") el false (unsafeCoerce e :: EventTarget)
     Nothing -> throw "No 'stop' button"
   pure unit
